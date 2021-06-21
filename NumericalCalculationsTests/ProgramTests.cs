4d7bba24abc27d798e4197f46b7ce60462528fbe
Hamsti <35877392+Hamsti@users.noreply.github.com>
2021-06-21 11:50:30 +0300

    Initial commit

---
+++ NumericalCalculationsTests/ProgramTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using NumericalCalculations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalCalculations.Tests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+    }
+}
---
+++ TextHandlerTests/ProgramTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using TextHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextHandler.Tests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+    }
+}
